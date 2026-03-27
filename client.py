3a26e8b0a5d1601e0aa386a22e3fdef80347bab1
Codebossman <erkasub@gmail.com>
2026-03-27 12:17:26 -0500

    Refactor motor setup and enhance control functions
    Refactor motor initialization and add new functions for pincers and grinder control. Adjust deadzone for speed input and clean up packet handling logic.

--- client.py
+++ client.py
@@ -2,36 +2,45 @@ from pybricks.hubs import PrimeHub
 from pybricks.pupdevices import Motor
 from pybricks.parameters import Port
 from pybricks.tools import wait
+from pybricks.parameters import Direction
 
 hub = PrimeHub(broadcast_channel=None, observe_channels=[1])
 
-leftMotor = Motor(Port.A)
-rightMotor = Motor(Port.B)
-leftFlick = Motor(Port.E)
-rightFlick = Motor(Port.F)
+leftMotor = Motor(Port.A,positive_direction=Direction.CLOCKWISE)
+rightMotor = Motor(Port.B,positive_direction=Direction.CLOCKWISE)
+leftFlick = Motor(Port.E, positive_direction=Direction.CLOCKWISE)
+rightFlick = Motor(Port.F,positive_direction=Direction.CLOCKWISE)
+pincers = Motor(Port.D,positive_direction=Direction.CLOCKWISE)
+grinder = Motor(Port.C, positive_direction=Direction.COUNTERCLOCKWISE)
+
+
 
 leftFlick.reset_angle()
 rightFlick.reset_angle()
 
 targetAttack = 25
+
 def run(speed, turn):
     # scale inputs
     speed = speed * 100
     turn  = turn * 100
 
     # deadzone (prevents drifting)
-    if abs(turn) < 5:
+    if abs(turn) < 2:
         turn = 0
+    if abs(speed < 5):
+        speed = 0
 
     # swerve math
     left  = speed + turn
     right = speed - turn
 
     # clamp values
-    left  = max(min(left, 100), -100)
+    left = max(min(left, 100),-100)
     right = max(min(right, 100), -100)
 
     # run motors (invert right side)
+    
     print(left, right)
     leftMotor.run(-left*100)
     rightMotor.run(right*100)
@@ -48,23 +57,48 @@ def flicker(target):
     leftFlick.run_target(1000, -100)
     rightFlick.run_target(1000, -100)
     
+def pince():
+    pincers.reset_angle(0)
+
+    pincers.run_target(1000, 60)
+    wait(0.1)
+    pincers.run_target(1000, 0)
+
+def grind(running):
+    if(running):
+        grinder.run(1000)
+    else:
+        grinder.brake()
+
+def checkButtons(packet):
+    if(packet[2] == True):
+        pince()
+        print(pincers.angle())
+    if(packet[3] == True):
+        flicker(targetAttack)
+        print(leftFlick.angle(), rightFlick.angle())
+    
+def grinderExBtn(packet):
+    grind(packet)
+
+def handleExit(packet):
+    if(packet[4] == True):
+        onExit()
+        running = False
+    elif(packet[4] == False):
+        running = True
 
 
 while True:
     packets = hub.ble.observe(1)    
     if(packets != None):
-        if(packets[4] == False):
+        #if(packets[4] == False):
 
-            print(packets)
-            run(packets[1], packets[0])
-            if(packets[3] == True):
-                flicker(targetAttack)
-                print(leftFlick.angle, rightFlick.angle)
-        if(packets[4] == True):
-            onExit()
-            running = False
-        elif():
-            running = True
+        print(packets[4])
+        run(packets[1], packets[0])
+        checkButtons(packets)
+        grinderExBtn(packets[4])
+
     else:
         print("No packets")
     wait(100)
